33de45200332ff8ef6bfaadf572e04a7a5b3877e
Galaxiealpha99 <galaxiealpha09@gmail.com>
2022-12-18 15:55:36 -0800

    5-print_numbers-6-print_numberz

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints the alphabet in lowercase.
+ *
+ * Return: 0 on success
+ */
+
+int main(void)
+{
+	int debut = 0;
+
+	for (debut = 0; debut < 10; debut++)
+	{
+		printf("%d",debut);
+	}
+	printf("\n");
+	return (0);
+}
+
+
---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints the alphabet in lowercase.
+ *
+ * Return: 0 on success
+ */
+
+int main(void)
+{
+	char debut = '0';
+
+	for (debut = '0'; debut <= '9'; debut++)
+	{
+		putchar(debut);
+	}
+	putchar('\n');
+	return (0);
+}
+
+
