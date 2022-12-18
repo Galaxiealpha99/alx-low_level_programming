a063980fddc948aa782a7fc4fd3a8d70777fe79d
Galaxiealpha99 <galaxiealpha09@gmail.com>
2022-12-18 14:48:30 -0800

    3-print_alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints the alphabet in lowercase.
+ *
+ * Return: 0 on success
+ */
+
+int main(void)
+{
+	char debut = 'a';
+	char maj = 'A';
+
+	for (debut = 'a'; debut <= 'z'; debut++)
+	{
+		putchar(debut);
+	}
+	for (maj = 'A'; maj <= 'Z'; maj++)
+	{
+		putchar(maj);
+	}
+	putchar('\n');
+	return (0);
+}
+
+
