63136cdc203f1585ca64513f9432def9b17e94da
Galaxiealpha99 <galaxiealpha09@gmail.com>
2023-01-03 07:40:39 -0800

    0-putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+char word = '_putchar';
+
+int main(void)
+{
+	char school[10] = "_putchar";
+
+	int i;
+
+	for (i = 0; i < 8; i++)
+		{
+			_putchar(school[i]);
+		}
+			_putchar(10);
+			return (0);
+}
