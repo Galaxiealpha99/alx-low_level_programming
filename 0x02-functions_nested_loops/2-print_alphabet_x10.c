16f24a2c2ed9a67233032038d3c52475dffd369b
Galaxiealpha99 <galaxiealpha09@gmail.com>
2023-01-03 17:41:57 -0800

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,14 +8,13 @@
 
 void print_alphabet_x10(void)
 {
+	int counter = 0;
 	int letter;
-	for (int i = 1; i <= 10; i++)
+	while (counter++ <= 9)
 	{
+		for (letter = 'a'; letter <= 'z'; letter++)
+			_putchar(letter);
 
-	for (letter = 'a'; letter <= 'z'; letter++)
-	{
-		_putchar (letter);
-	}
-	_putchar (10);
+		_putchar(10);
 	}
 }
