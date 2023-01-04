fdf568dd8a6ecdb5c01870140e768e9f5e884663
Galaxiealpha99 <galaxiealpha09@gmail.com>
2023-01-03 16:21:13 -0800

    0-putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,16 @@
 #include "main.h"
-#include <stdio.h>
-#include <stdlib.h>
-
-char word = '_putchar';
 
 int main(void)
 {
-	char school[10] = "_putchar";
 
-	int i;
-
-	for (i = 0; i < 8; i++)
-		{
-			_putchar(school[i]);
-		}
-			_putchar(10);
-			return (0);
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
 }
